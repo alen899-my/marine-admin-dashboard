1b38013d8a83ab0753872358387cd717a38d2296
RohithSekharN <rohithsekharnaduvath@gmail.com>
2026-03-09 13:01:35 +0530

    redirection undone

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,6 @@ const nextConfig: NextConfig = {
   reactStrictMode: false,
   output: "standalone",
 
-  async redirects() {
-    return [
-      {
-        // ":path*" matches everything, including nested routes
-        source: "/:path*", 
-        destination: "https://demo-maritime.parkora.io/:path*", 
-        permanent: true, // Set to true for a permanent SEO move (308 redirect)
-      },
-    ];
-  },
-
   images: {
     remotePatterns: [
       {
